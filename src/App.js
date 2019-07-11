d65a8b561c3a4b20103ba6374a57805bd49f5bd9
Greg Jordan <gregljordan@gmail.com>
2019-07-10 23:23:32 -0400

    Built the App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import Header from "./Components/Title";
+import Title from "./Components/Title";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    
+    date: '',
+    explanation: "",
+    media_type: "",
+    service_version: "",
+    title:"",
+    url:""
+  })
+
+  useEffect( () => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=gMsZfkpVIl4FWwGE0XSzk3NtyMt3ZZrM0gxmpBEd')
+    .then( res => {
+      console.log(res.data)
+      setState(res.data)
+    })
+    .catch(err => console.log("You know you messed up, right?", err))
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        <Title  title={state.title} />
+      <div className="pic-of-the-day">
+        {state.date}
+      </div>
     </div>
   );
 }
 
 export default App;
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
