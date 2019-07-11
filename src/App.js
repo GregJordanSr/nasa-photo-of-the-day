57310c4058e482321e8335ce029a62a86f5406ca
Greg Jordan <gregljordan@gmail.com>
2019-07-10 23:57:13 -0400

    Built Title component and Information component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect} from "react";
 import axios from 'axios';
 import "./App.css";
-import Header from "./Components/Title";
 import Title from "./Components/Title";
 
 const App = () => {
